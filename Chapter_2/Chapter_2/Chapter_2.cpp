b5fa5c33e0c0917866ee0033fe5b631d92ef7a41
Eliott <eliott_nu@mail.ru>
2021-07-17 10:48:30 +0900

    exercises 12 to 17 in Chapter 2

--- Chapter_2/Chapter_2/Chapter_2.cpp
+++ Chapter_2/Chapter_2/Chapter_2.cpp
@@ -35,10 +35,52 @@ void exercise11() {
     extern int iz; // declaration
 }
 
+void exercise12() {
+    // (a) int double = 3.14;
+    int _;
+    // (c) int catch-22;
+    // (d) int 1_or_2 = 1;
+    double Double = 3.14;
+}
+
+void exercise13() {
+    int i = 100, sum = 0;
+    for (int i = 0; i != 10; i++) {
+        sum += i;
+    }
+    cout << i << " " << sum << endl; // 100 45
+}
+
+void exercise15() {
+    // (a) int ival = 1.01;
+    int ival = 1;
+    // (b) int &rval1 = 1.01;
+    int &rval2 = ival;
+    // (d) int &rval3;
+}
+
+void exercise16() {
+    int i = 0, &r1 = i;
+    double d = 0, &r2 = d;
+
+    r2 = 3.14159; // d = r2; d = 3.14159
+    r2 = r1; // d = i(0); d = 0
+    i = r2; // i = d; i = 0;
+    r1 = d; // i = d; i = 0;
+    // i = 0, d = 0;
+}
+
+void exercise17() {
+    int i, &ri = i;
+    i = 5; ri = 10; // i = 10, ri = 10;
+    cout << i << " " << ri << endl;
+}
 
 int main()
 {
     cout << "Exercise 3: " << endl;  exercise3();
     cout << "Exercise 9: " << endl;  exercise9();
-
+    cout << "Exercise 13: " << endl;  exercise13();
+    cout << "Exercise 17: " << endl;  exercise17();
+ 
 }
