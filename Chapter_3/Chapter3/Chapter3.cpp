c91364d3517f69c68f17674935feee12c6eb22a5
Eliott <eliott_nu@mail.ru>
2021-07-21 14:52:47 +0900

    Exercise 1 in Chapter 3

---
+++ Chapter_3/Chapter3/Chapter3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Exercise 1 in Chapter 3
+int exercise1_9() {
+    int sum = 0, i = 50;
+    while (i < 100) {
+        sum += i;
+        i++;
+    }
+    return sum;
+}
+
+int main()
+{
+    cout << "Exercise 1: Sum = " << exercise1_9() << endl;
+}
+
