cf042ca078b549bac64d68f3b4dc5e2369e3ca2c
Eliott <eliott_nu@mail.ru>
2021-08-05 15:14:22 +0900

    Exercises 1-2 in Chapter 4

---
+++ Chapter_4/Chapter4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int exercise1() {
+    return 5 + 10 * 20 / 2; // 105
+}
+
+/* Exercise 2
+* vec.begin() = ((vec.begin)())
+* vec.begin() + 1 = ( * ((vec.begin)())) + 1
+*/
+   
+
+int main()
+{
+    cout << "Exercise 1: " << exercise1() << endl;
+}
