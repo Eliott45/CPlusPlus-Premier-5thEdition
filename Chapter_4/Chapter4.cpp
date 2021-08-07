4e408bd1d184d6bce01ab5c193d2b2d947df254c
Eliott <eliott_nu@mail.ru>
2021-08-07 18:32:21 +0900

    Exercises 3-7 in Chapter 4

--- Chapter_4/Chapter4.cpp
+++ Chapter_4/Chapter4.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <limits>
 
 using namespace std;
 
@@ -10,9 +11,45 @@ int exercise1() {
 * vec.begin() = ((vec.begin)())
 * vec.begin() + 1 = ( * ((vec.begin)())) + 1
 */
-   
+
+/* Exercise 3
+* I think that is an acceptable trade-off.
+*/
+
+
+int exercise4() {
+    return 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2; // 16 + 75 + 0 = 91
+    // ((12/3) * 4) + (5 * 15) + ((24 % 4) / 2) 
+}
+
+void exercise5() {
+    cout << -30 * 3 + 21 / 5 << endl; // (a) = -90 + 4.2 = -86
+    cout << -30 + 3 * 21 / 5<< endl;  // (b) = -30 + 12.6 = -18
+    cout << 30 / 3 * 21 % 5 << endl;  // (c) = 0
+    cout << -30 / 3 * 21 % 4 << endl; // (d) = -2
+}
+
+string exercise6() {
+    int i;
+    cout << "Enter the number: ";
+    cin >> i;
+    return (i % 2 == 0) ? "Even" : "Odd";
+}
+
+void exercise7() {
+    // Overflow is when the value of a variable is out of range. 
+    int a = numeric_limits<int>::max();
+    int b = numeric_limits<int>::min();
+    cout << a + 1 << endl;
+    cout << b - 1 << endl;
+}
+
 
 int main()
 {
     cout << "Exercise 1: " << exercise1() << endl;
+    cout << "Exercise 4: " << exercise4() << endl;
+    cout << "Exercise 5: " << endl; exercise5();
+    // cout << "Exercise 6: " << endl; cout << exercise6() << endl;
+    cout << "Exercise 7: " << endl; exercise7();
 }
