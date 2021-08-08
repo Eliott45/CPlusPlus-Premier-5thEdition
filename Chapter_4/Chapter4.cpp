896c5554bd4e1126b82204684280473b01f2c201
Eliott <eliott_nu@mail.ru>
2021-08-08 11:59:53 +0900

    Exercise 20 in Chapter 4

--- Chapter_4/Chapter4.cpp
+++ Chapter_4/Chapter4.cpp
@@ -131,6 +131,16 @@ void exercise15() {
 * c) vec[ival++] <= vec[ival] better ++ival, vec[ival] <= vec[ival + 1]
 */
 
+/* Exercise 20
+* vector<string>::iterator; 
+* (a) *iter++; is legal, the expression moves iter to point to the next element and returns the value of the original element.
+* (b) (*iter)++;  is not legal. The expression means increasing value of the element, but the value is a string and string does not have ++ operator.
+* (c) *iter.empty(); is not legal. Because iter is an iterator and has no member named empty.
+* (d) iter->empty(); is legal. The expression means check if the string pointed by iter is empty 
+* (e) ++*iter;  is not legal. The expression means increasing value of the element, but the value is a string and string does not have ++ operator.
+* (f) iter++->empty();  is legal. The expression means move iter to point to the next element and check if the original string is empty.
+*/
+
 int main()
 {
     cout << "Exercise 1: " << exercise1() << endl;
