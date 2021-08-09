e6509a9e4036e92c2bc01e37ace83f60789cef3a
Eliott <eliott_nu@mail.ru>
2021-08-09 13:29:07 +0900

    Exercises 5-8 in Chapter 5

--- Chapter_5/Chapter_5.cpp
+++ Chapter_5/Chapter_5.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -26,11 +28,63 @@ int exercise3() {
 /* Exercise 4 
 * (a) string::iterator iter = s.begin();
 * while (iter != s.end()) {...}
-* 
+* (b) bool status;
+* while (status = find(word)) {...}
+* if (!status) {...}
+*/
+
+string exercise5() {
+	const vector<string> scores = { "F", "D", "C", "B", "A", "A++" };
+	string letterGrade;
+	int grade;
+	cin >> grade;
+	if (grade >= 60) {
+		letterGrade = scores[(grade - 50) / 10];
+	}
+	else {
+		letterGrade = scores[0];
+	}
+	return letterGrade;
+}
+
+string exercise6() {
+	const vector<string> scores = { "F", "D", "C", "B", "A", "A++" };
+	int grade;
+	cin >> grade;
+	return grade < 60 ? scores[0] : scores[(grade - 50) / 10];
+}
+
+/* Exercise 7
+* (a) 
+*	if (ival1 != ival2)
+*		ival1 = ival2;
+*	else ival1 = ival2 = 0;
+* (b)
+* if (ival < minval) {
+*	minval = ival;
+*	occurs = 1; 
+* }
+* (c)
+* int ival;
+* if (ival = get value()) 
+*	cout << "ival = " << ival " << ival << endl;s
+* if(!ival) 
+*	cout << "ival = 0\n";
+* (d)
+* if (ival == 0) 
+*	ival = get_value();
+*/
+
+/* Exercise 8
+* A "dangling else" means the ambiguity that an else may belong to several if. 
+* In C++, this is resolved by specifying that each else is matched with the closest preceding unmatched if.
 */
 
 int main()
 {
 	cout << "Exercise 2: " << exercise2() << endl;
 	cout << "Exercise 3: " << exercise3() << endl;
+	// cout << "Exercise 5: " << exercise5() << endl;
+	// cout << "Exercise 6: " << exercise6() << endl;
+
 }
