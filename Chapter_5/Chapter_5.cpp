45dd1b3dfd53e3bcc6a0a2ba2ec912722086acde
Eliott <eliott_nu@mail.ru>
2021-08-09 13:07:31 +0900

    Exercises 1-3 in Chapter 5

---
+++ Chapter_5/Chapter_5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void exercise1() {
+	string s;
+	while (cin >> s && s != "Stop")
+		; // Empty operator  
+}
+
+int exercise2() {
+	int val = 1, sum = 0;
+	while (val <= 10) { // block
+		sum += val;
+		++val;
+	} // block
+	return sum;
+}
+
+int exercise3() {
+	int val = 1, sum = 0;
+	while (val <= 10 && (sum += val, ++val));
+	return sum;
+}
+
+int main()
+{
+	cout << "Exercise 2: " << exercise2() << endl;
+	cout << "Exercise 3: " << exercise3() << endl;
+}
