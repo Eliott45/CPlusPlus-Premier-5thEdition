38698a159614d35aa33c05db3b78f385cb46be54
Maksim <eliott_nu@mail.ru>
2021-09-29 21:20:42 +0900

    Exercises 2-5 in Chapter 7

--- Chapter_7/Chapter_7/Chapter_7.cpp
+++ Chapter_7/Chapter_7/Chapter_7.cpp
@@ -2,14 +2,24 @@
 
 using namespace std;
 
-#pragma region Exercise1
 
 struct Sales_data {
+    string isbn() const { return bookNo; }
+    Sales_data& combine(const Sales_data&);
+
     string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
 
+// exercise 2
+Sales_data& Sales_data::combine(const Sales_data& rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+// Exercise 1, 3
 string exercise1() {
     Sales_data total;
     double price;
@@ -18,20 +28,19 @@ string exercise1() {
         Sales_data trans;
         while (cin >> trans.bookNo >> trans.units_sold >> price) {
             trans.revenue = trans.units_sold * price;
-            if (total.bookNo == trans.bookNo) {
-                total.units_sold += trans.units_sold;
-                total.revenue += trans.revenue;
+            if (total.isbn() == trans.isbn()) {
+                total.combine(trans);
             }
             else {
                 cout << total.bookNo << " "
                     << total.units_sold << " "
                     << total.revenue << endl;
-                total.bookNo = trans.bookNo;
-                total.units_sold = trans.units_sold;
-                total.revenue = trans.revenue;
+                total = trans;  
             }
         }
-        cout << total.bookNo << " " << total.units_sold << " " total.revenue << endl;
+        cout << total.bookNo << " "
+            << total.units_sold << " "
+            << total.revenue << endl;
     }
     else {
         cerr << "No data!" << endl;
@@ -39,9 +48,14 @@ string exercise1() {
     }
 }
 
-#pragma endregion
+// Exercise 4-5
+struct Person {
+    string getName() const { return name; }
+    string getAddress() const { return address; }
 
-
+    string name;
+    string address;
+};
 
 
 int main()
