c52420325c39259162c2c90fd4406d91e03c59ad
Maksim <eliott_nu@mail.ru>
2021-09-29 20:57:33 +0900

    Exercise 1 in Chapter 7

---
+++ Chapter_7/Chapter_7/Chapter_7.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#pragma region Exercise1
+
+struct Sales_data {
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+string exercise1() {
+    Sales_data total;
+    double price;
+    if (cin >> total.bookNo >> total.units_sold >> price) {
+        total.revenue = total.units_sold * price;
+        Sales_data trans;
+        while (cin >> trans.bookNo >> trans.units_sold >> price) {
+            trans.revenue = trans.units_sold * price;
+            if (total.bookNo == trans.bookNo) {
+                total.units_sold += trans.units_sold;
+                total.revenue += trans.revenue;
+            }
+            else {
+                cout << total.bookNo << " "
+                    << total.units_sold << " "
+                    << total.revenue << endl;
+                total.bookNo = trans.bookNo;
+                total.units_sold = trans.units_sold;
+                total.revenue = trans.revenue;
+            }
+        }
+        cout << total.bookNo << " " << total.units_sold << " " total.revenue << endl;
+    }
+    else {
+        cerr << "No data!" << endl;
+        return "Error";
+    }
+}
+
+#pragma endregion
+
+
+
+
+int main()
+{
+    // exercise1();
+}
+
+
