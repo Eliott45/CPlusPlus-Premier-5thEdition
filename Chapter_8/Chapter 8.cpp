7ce71cf000bdd50b4f76e62c1fbd4269b653d6c4
Maksim <eliott_nu@mail.ru>
2021-10-13 21:22:18 +0900

    Exercises 4-5 in Chapter 8

--- Chapter_8/Chapter 8.cpp
+++ Chapter_8/Chapter 8.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -24,8 +26,34 @@ The condition of a stream is equivalent to !fail(), which means set either failb
     *   Reaching end-of-file. (Set both the eofbit and failbit)
 */
 
+/// <summary>
+/// Exercises 4-5
+/// </summary>
+vector<string> exercise4(const string& filename) {
+    ifstream in(filename);
+    vector<string> vs;
+    if (in) {
+        for (string s; getline(in, s);) {
+            vs.push_back(s);
+        }
+    }
+    else {
+        cerr << "Fail to open file: " << filename << endl;
+    }
+    return vs;
+}
+
 int main()
 {
-    exercise1(cin);
+    // exercise1(cin);
+
+    // Exercises 4-5
+
+    string filename;
+    cin >> filename;
+    auto vs = exercise4(filename);
+    for (decltype(vs.size()) i = 0; i != vs.size(); ++i) {
+        cout << i + 1 << "\t: " << vs[i] << endl;
+    }
 }
 
