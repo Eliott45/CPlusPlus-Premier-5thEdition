5f4274de6dbda311cb97c386fec717c7b17a00fd
Maksim <eliott_nu@mail.ru>
2021-10-18 17:00:56 +0900

    Exercises 3-6 in Chapter 9

--- Chapter_9/Chapter_9.cpp
+++ Chapter_9/Chapter_9.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
+#include <sstream>
 #include <list>
 #include <deque>
+#include <vector>
 
 using namespace std;
 
@@ -14,10 +16,54 @@ using namespace std;
 
 */
 
+/* Exercise 3
+The constraints for two iterators, begin and end, forming an iterator range are
+1. They refer to elements of, or one past the end of, the same container.
+2. end must not precede begin.
+*/
+
+bool exercise4(vector<int>::const_iterator begin, vector<int>::const_iterator end, int k) {
+    for (auto it = begin; it != end; ++it) {
+        if (k == *it) return true;
+    }
+    return false;
+}
+
+vector<int>::const_iterator exercise5(vector<int>::const_iterator begin, vector<int>::const_iterator end, int k) {
+    for (auto it = begin; it != end; ++it) {
+        if (k == *it) return it;
+    }
+    return end;
+}
+
+/* Exercise 6
+    The iterator of list does not support iterator arithmetic, including relational operators like >, <. Thus the condition for the while loop should be iter1 != iter2.
+*/
+
 int main()
 {
     // Exercise 2
     list<deque<int>> exercise2;
+
+    // Exercise 4
+    string str;
+    getline(cin, str);
+    istringstream iss(str);
+    vector<int> vi;
+    int k;
+    while (iss >> k) {
+        vi.push_back(k);
+    }
+    cin >> k;
+    cout << exercise4(vi.cbegin(), vi.cend(), k) << endl;
+    // Exercise 5
+    auto it = exercise5(vi.cbegin(), vi.cend(), k);
+    if (it == vi.end()) {
+        cout << "No found " << k << " in range" << endl;
+    }
+    else {
+        cout << "Found " << *it << " at position " << it - vi.cbegin() << endl;
+    }
 }
 
 
